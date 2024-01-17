a424336926298b4f8ee4cfd48186168725a901e6
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-01-17 10:32:41 +0000

    set up proxy for front and back

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect
+  
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
