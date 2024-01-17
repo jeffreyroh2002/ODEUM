bac3670d160656952780a38a97a8c474b4bcd2d7
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-01-17 10:48:03 +0000

    update app.js for simple connection

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from 'react'
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([])
 
-  useEffect
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
   
   return (
-    <div>App</div>
+    <div>
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        data.members.map((members, i) => (
+          <p key={i}>{members}</p>
+        ))
+      )}
+    </div>
   )
 }
 
