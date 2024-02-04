853973a50371975d6a90165e7ddb235683f57091
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-04 10:06:00 +0000

    change to entire app, not one page

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,22 +1,17 @@
-import React from "react"
-import Header from "./components/Header"
-import sample_audio from "./Audio/sample_audio.mp3"
-import AudioPlayer from './components/AudioPlayer';
-import Form from './components/Form';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import Questionnaire from './Questionnaire';
 
-import playIcon from './images/icons8-play-64.png'
-import pauseIcon from './images/icons8-pause-64.png'
-
-export default function App() {
-    return (
-        <div>
-            <Header />
-            <AudioPlayer 
-                src={sample_audio}
-                playIconPath = {playIcon}
-                pauseIconPath = {pauseIcon}
-            />
-            <Form/>
-        </div>
-    )
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/questionnaire" element={<Questionnaire />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
