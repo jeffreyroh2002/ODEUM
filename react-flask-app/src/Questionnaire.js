f9f0f9b06b6f8b99ff3d0da7f4b87cdd3cbb6167
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-04 10:06:26 +0000

    previous app.js, now only handle survey

---
+++ react-flask-app/src/Questionnaire.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Header from "./components/Header"
+import sample_audio from "./Audio/sample_audio.mp3"
+import AudioPlayer from './components/AudioPlayer';
+import Form from './components/Form';
+
+import playIcon from './images/icons8-play-64.png'
+import pauseIcon from './images/icons8-pause-64.png'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <AudioPlayer 
+                src={sample_audio}
+                playIconPath = {playIcon}
+                pauseIconPath = {pauseIcon}
+            />
+            <Form/>
+        </div>
+    )
+}
