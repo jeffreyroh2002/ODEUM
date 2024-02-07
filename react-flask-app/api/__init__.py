4bb9e55d155191cbc653d62e13a7830ccb7e5551
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-07 12:28:49 +0000

    adding csrf

--- react-flask-app/api/__init__.py
+++ react-flask-app/api/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect  # Import CSRFProtect
 from api.config import Config
 import os
 
@@ -13,6 +14,8 @@ login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
 mail = Mail()
 
+# Initialize CSRFProtect
+csrf = CSRFProtect()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -22,14 +25,17 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    
+    # Initialize CSRFProtect with the Flask app instance
+    csrf.init_app(app)
 
     # section for importing blueprints
-    #from api.users.routes import users
+    from api.users.routes import users
     from api.main.routes import main
     #from api.questions.routes import questions
     #from api.results.routes import results
 
-    #app.register_blueprint(users)
+    app.register_blueprint(users)
     app.register_blueprint(main)
     #app.register_blueprint(questions)
     #app.register_blueprint(results)
