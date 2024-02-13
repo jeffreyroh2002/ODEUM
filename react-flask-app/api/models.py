2da72705adbc289c2a7fd35c4e43e184ed5f1ead
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-13 04:42:54 +0000

    changed username to firstname

--- react-flask-app/api/models.py
+++ react-flask-app/api/models.py
@@ -12,7 +12,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=True)
     tests = db.relationship("Test", backref="subject", lazy=True)
