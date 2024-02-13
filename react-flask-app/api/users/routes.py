ab7a2d5f569071243320b8c0a1b8f18e2d1a0fac
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-13 04:43:46 +0000

    probs need to remove and add val to react

--- react-flask-app/api/users/routes.py
+++ react-flask-app/api/users/routes.py
@@ -1,74 +1,47 @@
-
-from flask import jsonify, request, Blueprint
-from api import db, bcrypt
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from api.models import User
 
-users = Blueprint('users', __name__)
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-@users.route("/users/register", methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-    confirm_password = data.get('confirm_password')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one')
 
-    # Validate input
-    if not username or not email or not password or not confirm_password:
-        return jsonify({"error": "All fields are required"}), 400
-    if password != confirm_password:
-        return jsonify({"error": "Passwords do not match"}), 400
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
-    # Check if user already exists
-    if User.query.filter_by(email=email).first():
-        return jsonify({"error": "User already exists"}), 400
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
-    # Hash password
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
 
-    # Create user
-    new_user = User(username=username, email=email, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "Registration successful"}), 201
-
-@users.route("/users/login", methods=['POST'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-
-    # Validate input
-    if not email or not password:
-        return jsonify({"error": "Email and password are required"}), 400
-
-    # Check if user exists
-    user = User.query.filter_by(email=email).first()
-    if not user or not bcrypt.check_password_hash(user.password, password):
-        return jsonify({"error": "Invalid email or password"}), 401
-
-    # Log in user (you need to implement your login mechanism)
-    # login_user(user)
-
-    return jsonify({"message": "Login successful"}), 200
-
-@users.route("/users/logout", methods=['POST'])
-def logout():
-    # Implement your logout mechanism here
-    return jsonify({"message": "Logout successful"}), 200
-
-@users.route("/users/account", methods=['GET', 'PUT'])
-def account():
-    if request.method == 'PUT':
-        data = request.get_json()
-        # Update account details (you need to implement this logic)
-        return jsonify({"message": "Account updated"}), 200
-    elif request.method == 'GET':
-        # Get account details (you need to implement this logic)
-        return jsonify({"message": "Get account details"}), 200
-
-@users.route("/users/mypage", methods=['GET'])
-def mypage():
-    # Return user's page (you need to implement this logic)
-    return jsonify({"message": "My Page"}), 200
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
