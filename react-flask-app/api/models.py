b9822c30796be057df50250a013e0b6d6e101dd2
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-17 14:56:44 +0000

    fix mistake

--- react-flask-app/api/models.py
+++ react-flask-app/api/models.py
@@ -12,7 +12,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20), unique=F, nullable=False)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=True)
     tests = db.relationship("Test", backref="subject", lazy=True)
