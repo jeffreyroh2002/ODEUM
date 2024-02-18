d55a74a7d1cc645087f3723bb2f8a2e347b28130
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-18 14:08:54 +0000

    hide signup button if logged in

--- react-flask-app/src/Home.js
+++ react-flask-app/src/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "./components/Header"
 import { Link } from 'react-router-dom';
 import './Home.css';
+import axios from 'axios';
+
 import Typist from 'react-typist';
+
 
 export default function Home() {
 
@@ -15,22 +18,34 @@ export default function Home() {
 
   */
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Fetch isLoggedIn status from backend when component mounts
+    axios.get('/isLoggedIn')
+      .then(response => {
+        setIsLoggedIn(response.data.isLoggedIn);
+      })
+      .catch(error => {
+        console.error('Error fetching isLoggedIn status:', error);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="home-container">
         <div className="main-content">
           <h1 className="main-title">ODEUM</h1>
-          <p className="sub-title">Discover your music personality</p>
+          <p className="sub-title">Unraveling the Personalized Tapestry of Musical Taste</p>
           <Link to="/questionnaire" className="cta-button">
             Take Free Music Personality Test
-          </Link>
-          <Link to="/Signup" className="cta-button">
-            Sign Up Now
-          </Link>
-          <Link to="/Database" className="cta-button">
-            See Database
-          </Link>
+            </Link>
+          {!isLoggedIn && (
+            <Link to="/Signup" className="cta-button">
+              Sign Up Now
+            </Link>
+          )}
         </div>
       </div>
     </div>
