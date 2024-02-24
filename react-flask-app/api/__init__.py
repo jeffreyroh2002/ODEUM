7789d31f554e394872efcd3a4b3c42c7fe073ab0
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-24 12:12:55 +0000

    add static folder path

--- react-flask-app/api/__init__.py
+++ react-flask-app/api/__init__.py
@@ -19,7 +19,7 @@ mail = Mail()
 csrf = CSRFProtect()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     app.config.from_object(Config)
 
     db.init_app(app)
