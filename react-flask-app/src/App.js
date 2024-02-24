d5e7907d9f136072aa2866b4d55e3fda6edf6ea9
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-02-24 05:41:10 +0000

    added routing to before test

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
-import Questionnaire from './Questionnaire';
 import Signup from './Signup';
 import Login from './Login'
+import Questionnaire from './Questionnaire';
+import BeforeTest from './BeforeTest';
+
 import Database from './Database';
 
 function App() {
@@ -11,9 +13,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} exact />
-        <Route path="/questionnaire" element={<Questionnaire />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/BeforeTest" element={<BeforeTest />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
         <Route path="/Database" element={<Database />} />
       </Routes>
     </Router>
