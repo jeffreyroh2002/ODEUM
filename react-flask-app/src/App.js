bee01c5b5852b82f64ef76ff2f24658b47fa1a4b
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-03-01 10:51:06 +0000

    remove unneccessary

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Signup from './Signup';
 import Login from './Login'
