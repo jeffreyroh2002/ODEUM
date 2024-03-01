fe5566f3eceb7afc15c7befc9c07aa513f67691d
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-03-01 10:49:08 +0000

    first draft for test completed wip

--- react-flask-app/api/main/routes.py
+++ react-flask-app/api/main/routes.py
@@ -8,6 +8,24 @@ import re  # for email confirmation
 from datetime import datetime
 import logging
 
+from sklearn.preprocessing import normalize
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from collections import defaultdict
+import statistics
+import matplotlib.pyplot as plt
+
+#imports for saving png files
+import io
+import base64
+import matplotlib.pyplot as plt
+from flask import render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.ticker import MaxNLocator
+from collections import defaultdict
+import seaborn as sns
+
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -318,4 +336,259 @@ def get_user_info():
         'tests_data': tests_data
     })
 
+@main.route("/test_results", methods=['GET'])
+@login_required
+def test_results():
+    test_id = request.args.get('testId')
 
+    #calculate all characteristics
+    user = current_user
+    test = Test.query.filter_by(id=test_id).first()
+    if test.subject != current_user: 
+        return jsonify({'error': 'User does not match test owner'}), 403
+
+    display_messages = []
+    
+    #Update Preference each Song
+    genre_score = {'Rock': 0,'Hip Hop': 0,'Pop Ballad': 0,'Electronic': 0,'Korean Ballad': 0,'Jazz': 0,'R&B/Soul': 0}
+    mood_score = {'Tense': 0, 'Bright': 0, 'Emotional': 0, 'Relaxed': 0}
+    vocal_score = {'Smooth': 0,'Dreamy': 0,'Raspy': 0,'Voiceless': 0}
+
+    # High Rating song tracker
+    high_rated_songs = []
+
+    answers = UserAnswer.query.filter_by(test_id=test_id).all()
+    for answer in answers:
+        audio = AudioFile.query.get(answer.audio_id)
+
+        #from audioFile model
+        genre_pred = audio.genre   #assuming audioFile is populated with scores using dictionaries with same keys
+        mood_pred = audio.mood
+        vocal_pred = audio.vocal
+
+        #from Questions Form
+        overall_rating = answer.overall_rating   # need to use this
+        genre_rating = answer.genre_rating
+        mood_rating = answer.mood_rating
+        vocal_rating = answer.vocal_timbre_rating
+
+        #Calculate overall score
+        #Calculate each genre score
+        genre_weighted = {}
+        if genre_pred and not isinstance(audio.genre, float):
+            genre_scores = audio.genre
+            try:
+                genre_data = json.loads(genre_scores)
+            except json.JSONDecodeError:
+                print("Error decoding genre_data JSON.")
+                return jsonify({'error: Error decoding genre_data JSON'}), 404 
+
+            #if user is not sure about the genre, calculate the genre_score based on overall rating
+            if genre_rating == 0:
+                for genre_name, proportion in genre_data.items():
+                    genre_weighted[genre_name] = proportion * overall_rating
+            
+            #in other case, calculate the genre_score based on (0.3 portion of overall_rating) and (0.7 portion of genre_rating)
+            else:
+                for genre_name, proportion in genre_data.items():
+                    genre_weighted[genre_name] = (proportion * overall_rating * 0.3) + (proportion * genre_rating * 0.7)
+
+            for genre in genre_weighted:
+                genre_score[genre] += genre_weighted[genre]
+
+        else:
+            print("Genre data is not available or is in an unexpected format.")
+            return jsonify({'error: Genre data is not available or is in an unexpected format'}), 404 
+
+        #Calculate each mood score
+        mood_weighted = {}
+        if mood_pred and not isinstance(audio.mood, float):
+            mood_scores = audio.mood
+            try:
+                mood_data = json.loads(mood_scores)
+            except json.JSONDecodeError:
+                print("Error decoding mood_data JSON.")
+                return jsonify({'error: Error decoding mood_data JSON'}), 404 
+
+            #if user is not sure about the mood, calculate the mood_score based on overall rating
+            if mood_rating == 0:
+                for mood_name, proportion in mood_data.items():
+                    mood_weighted[mood_name] = proportion * overall_rating
+            
+            #in other case, calculate the mood_score based on (0.3 portion of overall_rating) and (0.7 portion of mood_rating)
+            else:
+                for mood_name, proportion in mood_data.items():
+                    mood_weighted[mood_name] = (proportion * overall_rating * 0.3) + (proportion * mood_rating * 0.7)
+
+            for mood in mood_weighted:
+                mood_score[mood] += mood_weighted[mood]
+
+        else:
+            print("Mood data is not available or is in an unexpected format.")
+            return jsonify({'error: Mood data is not available or is in an unexpected format'}), 404 
+
+        #Calculate each vocal timbre score
+        vocal_weighted = {}
+        if vocal_pred and not isinstance(audio.vocal, float):
+            vocal_scores = audio.vocal
+            try:
+                vocal_data = json.loads(vocal_scores)
+            except json.JSONDecodeError:
+                print("Error decoding vocal_data JSON.")
+                return jsonify({'error: Error decoding vocal_data JSON'}), 404 
+
+            #if user is not sure about the vocal, calculate the vocal_score based on overall rating
+            if vocal_rating == 0:
+                for vocal_name, proportion in vocal_data.items():
+                    vocal_weighted[vocal_name] = proportion * overall_rating
+            
+            #in other case, calculate the vocal_score based on (0.3 portion of overall_rating) and (0.7 portion of vocal_rating)
+            else:
+                for vocal_name, proportion in vocal_data.items():
+                    vocal_weighted[vocal_name] = (proportion * overall_rating * 0.3) + (proportion * vocal_rating * 0.7)
+
+            for vocal in vocal_weighted:
+                vocal_score[vocal] += vocal_weighted[vocal]
+
+        else:
+            print("Vocal data is not available or is in an unexpected format.")
+            return jsonify({'error: Vocal data is not available or is in an unexpected format'}), 404 
+        
+    # store highly rated songs into high_rated songs list
+        if (overall_rating >= 2):
+            high_rated_songs.append(answer.audio_id)
+
+    # array of arrays to store each feature vector of highly rated songs
+    high_rated_feature_vectors = []
+    for high_rated_song in high_rated_songs:
+        audio = AudioFile.query.get(high_rated_song)
+        genre_data = json.loads(audio.genre)
+        mood_data = json.loads(audio.mood)
+        vocal_data = json.loads(audio.vocal)
+
+        # Flatten the dictionaries into a single array
+        feature_vector = list(genre_data.values()) + list(mood_data.values()) + list(vocal_data.values())
+
+        # Normalize the feature vector
+        normalized_vector = normalize([feature_vector])[0]
+        high_rated_feature_vectors.append(normalized_vector)
+
+    
+    ## ALGORITHM FOR CUSTOM BINNING HISTORGRAM ##
+
+    # Initialize a dictionary to store all values for each attribute
+    attribute_values = defaultdict(list)
+
+    # Collect values for each attribute across all high-rated songs
+    for vector in high_rated_feature_vectors:
+        for attr_index, value in enumerate(vector):
+            attribute_name = get_attribute_name(attr_index)
+            attribute_values[attribute_name].append(value)
+
+    # Determine the most populated range for each attribute
+    attribute_ranges = {}
+    range_size = 0.2  # Define the size of each range
+
+    for attr, values in attribute_values.items():
+        # Filter out values less than or equal to 0.2
+        filtered_values = [value for value in values if value > 0.2]
+
+        # Bin values into ranges starting from 0.2
+        bins = np.arange(0.2, 1 + range_size, range_size)
+        hist, bin_edges = np.histogram(filtered_values, bins=bins)
+
+        # Find the range with the maximum count
+        max_count_index = np.argmax(hist)
+        common_range = (bin_edges[max_count_index], bin_edges[max_count_index + 1])
+        
+        attribute_ranges[attr] = common_range
+
+
+    # Prepare the display message
+    display_messages.append("Most common ranges for attributes in your top-rated songs (excluding 0 to 0.2):")
+    for attr, common_range in attribute_ranges.items():
+        display_messages.append(f"- {attr}: Most Common Range {common_range}")
+
+
+    # PLOTTING DENSITY PLOT
+
+    # Initialize a dictionary to store all values for each attribute
+    attribute_values = defaultdict(list)
+
+    # Collect values for each attribute across all high-rated songs
+    for vector in high_rated_feature_vectors:
+        for attr_index, value in enumerate(vector):
+            attribute_name = get_attribute_name(attr_index)
+            if value > 0.2:  # Exclude values between 0.0 and 0.2
+                attribute_values[attribute_name].append(value)
+
+    # Define color palettes for each category
+    genre_colors = sns.color_palette('Set1', len(genre_score))
+    mood_colors = sns.color_palette('Set2', len(mood_score))
+    vocal_colors = sns.color_palette('Set3', len(vocal_score))
+
+    
+    # Create a combined density plot for each category
+
+    # Plot for Genres
+    plt.figure(figsize=(5, 3))
+    for (genre, _), color in zip(genre_score.items(), genre_colors):
+        sns.kdeplot(attribute_values[genre], label=genre, color=color, fill=True, bw_adjust=0.5)
+    plt.title('Density Plots for Genres', fontsize=14)
+    plt.xlabel('Attribute Values', fontsize=12)
+    plt.ylabel('Density', fontsize=12)
+    plt.legend()
+    plt.tight_layout()
+    # Save the first plot
+    genre_png = io.BytesIO()
+    plt.savefig(genre_png, format='png')
+    genre_png.seek(0)
+    genre_encoded = base64.b64encode(genre_png.getvalue()).decode('utf-8')
+    plt.close()
+
+    # Plot for Moods
+    plt.figure(figsize=(5, 3))
+    for (mood, _), color in zip(mood_score.items(), mood_colors):
+        sns.kdeplot(attribute_values[mood], label=mood, color=color, fill=True, bw_adjust=0.5)
+    plt.title('Density Plots for Moods', fontsize=14)
+    plt.xlabel('Attribute Values', fontsize=12)
+    plt.ylabel('Density', fontsize=12)
+    plt.legend()
+    plt.tight_layout()
+    # Save the second plot
+    mood_png = io.BytesIO()
+    plt.savefig(mood_png, format='png')
+    mood_png.seek(0)
+    mood_encoded = base64.b64encode(mood_png.getvalue()).decode('utf-8')
+    plt.close()
+
+    # Plot for Vocals
+    plt.figure(figsize=(5, 3))
+    for (vocal, _), color in zip(vocal_score.items(), vocal_colors):
+        sns.kdeplot(attribute_values[vocal], label=vocal, color=color, fill=True, bw_adjust=0.5)
+    plt.title('Density Plots for Vocals', fontsize=14)
+    plt.xlabel('Attribute Values', fontsize=12)
+    plt.ylabel('Density', fontsize=12)
+    plt.legend()
+    plt.tight_layout()
+    # Save the third plot
+    vocal_png = io.BytesIO()
+    plt.savefig(vocal_png, format='png')
+    vocal_png.seek(0)
+    vocal_encoded = base64.b64encode(vocal_png.getvalue()).decode('utf-8')
+    plt.close()
+
+    # Pass the encoded images and other necessary information to the template
+    response_data = {
+        'user_id': user.id,
+        'test_id': test.id,
+        'test_type': test.test_type,
+        'genre_score': genre_score,
+        'mood_score': mood_score,
+        'vocal_score': vocal_score,
+        'genre_image': genre_encoded,
+        'mood_image': mood_encoded,
+        'vocal_image': vocal_encoded,
+        'display_messages': display_messages
+    }
+    return jsonify(response_data)
--- react-flask-app/src/TestCompleted.js
+++ react-flask-app/src/TestCompleted.js
@@ -1,18 +1,240 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Header from "./components/Header"
-import './TestCompleted.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from "./components/Header";
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 export default function TestCompleted() {
     const location = useLocation();
     const testId = new URLSearchParams(location.search).get('testId');
+    const [testResults, setTestResults] = useState(null);
+
+    // Refs for the charts to manage instances
+    const genreChartRef = useRef(null);
+    const moodChartRef = useRef(null);
+    const vocalChartRef = useRef(null);
+
+    useEffect(() => {
+        if (!testId) {
+            console.error('Test ID is missing.');
+            return;
+        }
+
+        fetch(`/test_results?testId=${testId}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setTestResults(data);
+            })
+            .catch(error => console.error('Error fetching test results:', error));
+    }, [testId]);
+
+    useEffect(() => {
+        if (testResults) {
+            initializeCharts();
+        }
+
+        // Cleanup function to destroy charts on component unmount or before reinitializing
+        return () => {
+            if (genreChartRef.current) {
+                genreChartRef.current.destroy();
+            }
+            if (moodChartRef.current) {
+                moodChartRef.current.destroy();
+            }
+            if (vocalChartRef.current) {
+                vocalChartRef.current.destroy();
+            }
+        };
+    }, [testResults]); // This useEffect depends on testResults
+
+    const initializeCharts = () => {
+        const genreCtx = document.getElementById('genreRadarChart').getContext('2d');
+        const moodCtx = document.getElementById('moodRadarChart').getContext('2d');
+        const vocalCtx = document.getElementById('vocalRadarChart').getContext('2d');
+
+        // Destroy existing charts if they exist
+        if (genreChartRef.current) genreChartRef.current.destroy();
+        if (moodChartRef.current) moodChartRef.current.destroy();
+        if (vocalChartRef.current) vocalChartRef.current.destroy();
+
+        // Initialize new chart instances with example data and options
+        genreChartRef.current = new Chart(genreCtx, {
+            type: 'radar',
+            data: {
+                labels: Object.keys(testResults.genre_score),
+                datasets: [{
+                    label: 'Genre Preferences',
+                    data: Object.values(testResults.genre_score),
+                    fill: true,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    pointBackgroundColor: 'rgb(255, 99, 132)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(255, 99, 132)'
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 3
+                    }
+                },
+                scales: {
+                    r: {
+                        angleLines: {
+                            display: false
+                        },
+                        suggestedMin: 0,
+                        suggestedMax: 10
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Genre Preferences'
+                    }
+                }
+            },
+        });
+
+        moodChartRef.current = new Chart(moodCtx, {
+            type: 'radar',
+            data: {
+                labels: Object.keys(testResults.mood_score),
+                datasets: [{
+                    label: 'Mood Preferences',
+                    data: Object.values(testResults.mood_score),
+                    fill: true,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgb(54, 162, 235)',
+                    pointBackgroundColor: 'rgb(54, 162, 235)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(54, 162, 235)'
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 3
+                    }
+                },
+                scales: {
+                    r: {
+                        angleLines: {
+                            display: false
+                        },
+                        suggestedMin: 0,
+                        suggestedMax: 10
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Mood Preferences'
+                    }
+                }
+            },
+        });
+
+        vocalChartRef.current = new Chart(vocalCtx, {
+            type: 'radar',
+            data: {
+                labels: Object.keys(testResults.vocal_score),
+                datasets: [{
+                    label: 'Vocal Timbre Preferences',
+                    data: Object.values(testResults.vocal_score),
+                    fill: true,
+                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                    borderColor: 'rgb(255, 206, 86)',
+                    pointBackgroundColor: 'rgb(255, 206, 86)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(255, 206, 86)'
+                }]
+            },
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 3
+                    }
+                },
+                scales: {
+                    r: {
+                        angleLines: {
+                            display: false
+                        },
+                        suggestedMin: 0,
+                        suggestedMax: 10
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Vocal Timbre Preferences'
+                    }
+                }
+            },
+        });
+    };
+
+    // Function to render preference scales (if applicable)
+    const renderPreferenceScales = (scores, title) => {
+        return (
+            <div>
+                <h3>{title} Preferences:</h3>
+                <ul>
+                    {Object.entries(scores).map(([key, value]) => (
+                        <li key={key}>{key}: {value}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    };
 
     return (
         <div>
             <Header />
             <h1>Test Completed</h1>
             <p>Test ID: {testId}</p>
-            {/* You can use the testId value as needed */}
+            {testResults && (
+                <div>
+                    {/* Radar charts for genre, mood, and vocal preferences */}
+                    <canvas id="genreRadarChart"></canvas>
+                    <canvas id="moodRadarChart"></canvas>
+                    <canvas id="vocalRadarChart"></canvas>
+                    {/* Displaying preference scales */}
+                    {renderPreferenceScales(testResults.genre_score, "Genre")}
+                    {renderPreferenceScales(testResults.mood_score, "Mood")}
+                    {renderPreferenceScales(testResults.vocal_score, "Vocal")}
+                    {/* Displaying messages */}
+                    <div className="display-messages">
+                        {testResults.display_messages && testResults.display_messages.length > 0 ? (
+                            testResults.display_messages.map((message, index) => (
+                                <p key={index}>{message}</p>
+                            ))
+                        ) : (
+                            <p>There are no specific messages to display.</p>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
