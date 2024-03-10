426a9c0323b934b91024ccccb6ffcf12c7f4ce0a
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-03-10 15:05:48 +0000

    header component styling

--- react-flask-app/src/components/Header.js
+++ react-flask-app/src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import axios from 'axios';
-import logo from '../images/logo.png'
+import full_logo from '../images/full_logo.png'
 import account_icon from '../images/account_circle_filled_white_24px.png'
 import menu_icon from '../images/menu_white_24px.png'
 
@@ -33,7 +33,7 @@ export default function Header() {
         <header className="header">
             <Link to="/">
             <img 
-                src={logo} 
+                src={full_logo} 
                 alt="ODEUM" 
                 className="header--image"
             />
