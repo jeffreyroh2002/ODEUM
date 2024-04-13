ff9d195b3e073f2522e8950f81179b64931d14b5
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-04-13 07:29:03 +0000

    update questions

--- react-flask-app/api/presurvey/routes.py
+++ react-flask-app/api/presurvey/routes.py
@@ -8,13 +8,23 @@ presurvey = Blueprint('presurvey', __name__)
 questions = [
     {
         "id": 1,
-        "text": "What is your favorite color?",
-        "answers": ["Red", "Blue", "Green", "Yellow"]
+        "text": "Are you aware of your musical preferences?",
+        "answers": ["Yes", "No"]
     },
     {
         "id": 2,
-        "text": "What is your preferred pet?",
-        "answers": ["Dog", "Cat", "Bird", "Fish"]
+        "text": "Would you like to discover new music or find tracks similar to what you already enjoy?",
+        "answers": ["Explore new style of music", "Find similar music to my current favorites"]
+    },
+    {
+        "id": 3,
+        "text": "What genres of music do you prefer?",
+        "answers": ["Pop", "Rock", "Hip-Hop", "Jazz", "Electronic", "R&B", "Indie"]
+    },
+    {
+        "id": 4,
+        "text": "Do you listen to music based on specific contexts or situations, or more casually?",
+        "answers": ["Based on contexts or situations", "More casual"]
     }
 ]
 
