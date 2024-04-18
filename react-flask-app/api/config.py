250d5100c821477345c838dffe1a4bcf3cef5845
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-04-18 14:47:48 +0000

    development mode comment

--- react-flask-app/api/config.py
+++ react-flask-app/api/config.py
@@ -8,7 +8,7 @@ if uri and uri.startswith("postgres://"):
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '9a96279c05c8abc02388d6faba5654f5')
     SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///site.db'
-    SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+    SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')  #need to set up .env for development mode
     SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
     REDIRECT_URI = os.getenv('REDIRECT_URI', 'https://jeffreyroh2002-odeum-ol6y5xcofwj.ws-us110.gitpod.io/callback')
 
