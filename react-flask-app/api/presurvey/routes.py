fa784cf189c9f6dd25faeab2770b4ba84611cc8d
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-04-19 12:03:13 +0000

    when test == None, create new test (same logic as beforeTest)

--- react-flask-app/api/presurvey/routes.py
+++ react-flask-app/api/presurvey/routes.py
@@ -5,7 +5,8 @@ import requests
 import os
 import base64
 import json
-from flask_login import login_required, current_user 
+from flask_login import login_required, current_user
+from datetime import datetime
 
 
 presurvey = Blueprint('presurvey', __name__)
@@ -52,23 +53,32 @@ def process_presurvey_questions():
         # Fetch the most recent test for the current user
         test = Test.query.filter_by(user_id=current_user.id).order_by(Test.test_start_time.desc()).first()
         
-        if test is not None:
-            # Assuming pre_survey_data is a JSON-formatted text field that needs to be updated
-            if test.pre_survey_data:
-                # Decode existing answers if available
-                existing_answers = json.loads(test.pre_survey_data)
-            else:
-                existing_answers = {}
-            
-            # Update the answers for the specific question
-            existing_answers[str(question_id)] = answers
-            
-            # Serialize and save back to the test
-            test.pre_survey_data = json.dumps(existing_answers)
+        if not test or test.test_end_time:        
+            test_val = Test(
+                test_type = 1,
+                test_start_time = datetime.now(),
+                subject = current_user
+            )
+            db.session.add(test_val)
             db.session.commit()
-            print("Here is what is commited to DB!!!:", test.pre_survey_data)
-            return jsonify({"message": "Pre-survey answers updated successfully!", "test_id": test.id}), 200
+            test = Test.query.filter_by(user_id=current_user.id, test_type=1).order_by(Test.test_start_time.desc()).first()
+            audio_id = 1
+
+        # Assuming pre_survey_data is a JSON-formatted text field that needs to be updated
+        if test.pre_survey_data:
+            # Decode existing answers if available
+            existing_answers = json.loads(test.pre_survey_data)
         else:
-            return jsonify({"error": "No test found for the user"}), 404
+            existing_answers = {}
+        
+        # Update the answers for the specific question
+        existing_answers[str(question_id)] = answers
+        
+        # Serialize and save back to the test
+        test.pre_survey_data = json.dumps(existing_answers)
+        db.session.commit()
+        print("Here is what is commited to DB!!!:", test.pre_survey_data)
+        return jsonify({"message": "Pre-survey answers updated successfully!", "test_id": test.id}), 200
+            
     else:
         return jsonify({"error": "Method not allowed"}), 405
