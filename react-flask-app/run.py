66fe5166761c4d783573cf8a6b221ffb88945d81
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-04-19 14:30:25 +0000

    update so production turns off debugging mode

--- react-flask-app/run.py
+++ react-flask-app/run.py
@@ -1,5 +1,6 @@
 from api import create_app, db
 from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -7,4 +8,6 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    debug = os.environ.get("FLASK_ENV") == "development"
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=debug)
