197427059dbb42fa426249a29cfb9715ea1e2f73
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-04-19 14:44:06 +0000

    conditional to use gunicorn if for production

--- react-flask-app/run.py
+++ react-flask-app/run.py
@@ -8,6 +8,15 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    debug = os.environ.get("FLASK_ENV") == "development"
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    if os.environ.get("FLASK_ENV") == "production":
+        # Determine the number of worker processes based on available CPU cores
+        num_workers = os.cpu_count() * 2 + 1 if os.cpu_count() else 1
+
+        # Start the Gunicorn server
+        bind_address = '0.0.0.0:' + str(os.environ.get("PORT", 5000))
+        os.system(f'gunicorn -w {num_workers} -b {bind_address} "run:create_app()"')
+    else:
+        # For development, run the Flask development server
+        debug = True
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port, debug=debug)
