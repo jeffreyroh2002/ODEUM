6626afb4626a8bf314492eaef898332e0c0f8258
donghoon Ko <dongdong1653@gmail.com>
2024-04-27 05:25:00 +0000

    fix several bugs in audioplayer

--- react-flask-app/frontend/src/components/streaming_AudioPlayer.js
+++ react-flask-app/frontend/src/components/streaming_AudioPlayer.js
@@ -5,25 +5,33 @@ import './streaming_AudioPlayer.css'
 export default function Streaming_AudioPlayer({audioName, playIconPath, pauseIconPath}) {
     const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-    // this component's src will be designated later
     const audioRef = useRef(new Audio());
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [loadingAudio, setLoadingAudio] = useState(true);
 
+    // this component's src will be designated later
     useEffect(() => {
         setLoadingAudio(true);
+        audioRef.current.pause(); // Pause the audio immediately when loading a new one
         axios.get(`${BASE_URL}/stream_audio?audio_name=${audioName}`)
-             .then(response => {
-                audioRef.current.src = response.data.url;
+            .then(response => {
+                // Stop the audio playback before setting the new src
                 audioRef.current.pause();
                 setIsPlaying(false);
                 setCurrentTime(0);
+
+                // Set the new src and load the new audio
+                audioRef.current.src = response.data.url;
                 audioRef.current.load(); 
-              })
-             .catch(error => console.error('Error fetching audio URL:', error))
-             .then(setLoadingAudio(false));      
-    }, [audioName])
+
+                setLoadingAudio(false);
+            })
+            .catch(error => {
+                console.error('Error fetching audio URL:', error);
+                setLoadingAudio(false);
+            });    
+    }, [audioName]);
 
     const togglePlayPause = () => {
         if (!loadingAudio) {
