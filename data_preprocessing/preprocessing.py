f56450174a87a24e5328ec054b35dc72c23534a3
Jeffrey Roh <jeffreyroh2002@gmail.com>
2024-05-03 14:27:50 +0000

    adjustments to preprocessing.py

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -3,6 +3,8 @@ import os
 import librosa
 import json
 import math
+import glob
+import demucs.separate
 
 from pydub import AudioSegment
 from datetime import datetime  # Import the datetime module
@@ -17,98 +19,153 @@ SAMPLE_RATE = 22050
 DURATION = 30
 SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
 
+def convert_mp3_to_wav(mp3_file_path, wav_file_path):
+    # Load the MP3 file
+    audio = AudioSegment.from_mp3(mp3_file_path)
+    
+    # Export the audio data to a WAV file
+    audio.export(wav_file_path, format="wav")
+    
+    os.remove(mp3_file_path)
+
+def separate_audios_and_upload():
+    audios = glob.glob("non_split_audio/full-audio/*")
+
+    storage_client = storage.Client.from_service_account_json('/workspace/ODEUM/react-flask-app/backend/api/odeum-421210-44f51ed247e8.json')
+    bucket = storage_client.get_bucket("odeum-musics")
+
+    for audio_filepath in audios:
+        #if the extension is ".mp3", converting it to ".wav"
+        if audio_filepath[-4:] == ".mp3":
+            convert_mp3_to_wav(audio_filepath, audio_filepath[:-4] + ".wav")
+            audio_filepath = audio_filepath[:-4] + ".wav"
+
+        #separating music into vocal/no_vocal with "Demucs"
+        demucs.separate.main(["--mp3", "--two-stems", "vocals", "-n", "mdx", audio_filepath])
+        
+        #filename without extensions(.wav, .mp3 ...)
+        audio_filename = audio_filepath.split('/')[-1][:-4]
+
+        #paths of separated audios that will be saved later (yet, the extension is .mp3) 
+        no_vocals_path = "separated/mdx/" + audio_filename + "/no_vocals.wav"
+        vocals_path = "separated/mdx/" + audio_filename + "/vocals.wav"
+
+        if not os.path.exists(no_vocals_path) or not os.path.exists(vocals_path):
+            convert_mp3_to_wav(no_vocals_path[:-4] + ".mp3", no_vocals_path)
+            convert_mp3_to_wav(vocals_path[:-4] + ".mp3", vocals_path)
+            
+        #the names of blobs that will be uploaded to gcs
+        full_audio_blob = bucket.blob("non_split_audio/full-audio/" + audio_filename + ".wav")
+        no_vocals_blob = bucket.blob("non_split_audio/instrumental-audio/" + audio_filename + ".wav")
+        vocals_blob = bucket.blob("non_split_audio/vocal-audio/" + audio_filename + ".wav")
+
+        #uploading to gcs...    
+        full_audio_blob.upload_from_filename(audio_filepath)
+        no_vocals_blob.upload_from_filename(no_vocals_path)
+        vocals_blob.upload_from_filename(vocals_path)
+        
+        #removing audios which are already uploaded to gcs
+        os.remove(audio_filepath)
+        os.remove(no_vocals_path)
+        os.remove(vocals_path)
+    
+
 def count_audios(directory):
     with os.scandir(directory) as entries:
         return sum(1 for entry in entries if entry.is_file())
 
-def split_wav_into_segments():
-    print(os.getcwd())
-    #a directory of full-length audios 
-    non_split_audio_directory = os.path.join("non_split_audio")
-    if not os.path.exists(non_split_audio_directory):
-        raise FileNotFoundError("non split audio path does not exist")
+def download_blob(bucket, source, destination):
+    blob = bucket.blob(source)
+    blob.download_to_filename(destination)
 
-    #a directory where we split audios
-    split_audio_directory = os.path.join("split_audio")
-    os.makedirs(split_audio_directory, exist_ok=True)
+def read_data(file_path):
+    data_dict = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split("/")
+            if len(parts) == 3:
+                file_name, start, end = parts
+                data_dict[file_name] = (int(start), int(end))
+    return data_dict
 
-    timestamps_path = os.path.join("timestamps.txt")
-    if not os.path.exists(timestamps_path):
-        raise FileNotFoundError("timestamps file does not exist")
+def find_file_info(data_dict, file_name):
+    return data_dict.get(file_name, False)
 
-    full_audio_directory = os.path.join(non_split_audio_directory, "full-audio")
-    instrumental_audio_directory = os.path.join(non_split_audio_directory, "instrumental-audio")
-    vocal_audio_directory = os.path.join(non_split_audio_directory, "vocal-audio")
-
-    # Ensure the length of the txt file and the num audios are equal
-    with open(timestamps_path, 'r') as file:
-        line_count = sum(1 for line in file)
-
-    full_audio_count = count_audios(full_audio_directory)
-    instrumental_audio_count = count_audios(instrumental_audio_directory)
-    vocal_audio_count = count_audios(vocal_audio_directory)
-
-    assert full_audio_count == instrumental_audio_count and full_audio_count == vocal_audio_count \
-           and full_audio_count == line_count
-
-    # Read the input file and process each line 
-    with open(timestamps_path, "r") as timestamps:
-        for timestamp in timestamps:
-            data = timestamp.strip().split(",")
-            song_name, start_time, end_time = data[0], int(data[1]) * 1000, int(data[2]) * 1000
-            print(song_name, start_time, end_time)
-            
-            for audio_type in ["full-audio", "instrumental-audio", "vocal-audio"]:
-                audio_path = os.path.join(non_split_audio_directory, audio_type, song_name)
-                try:
-                    audio = AudioSegment.from_wav(audio_path)
-                except:
-                    print(f"an error occurred when getting audio {song_name}")
-                
-                segment = audio[start_time:end_time]
-                
-                output_path = os.path.join(split_audio_directory, audio_type)
-                os.makedirs(output_path, exist_ok=True)
-                
-                output_file = os.path.join(output_path, song_name)
-        
-                segment.export(output_file, format="wav")
-
-
-
-def before_preprocessing():
-    full_audio_path = 'non_split_audio/full-audio/'
-    instrumental_audio_path = 'non_split_audio/instrumental-audio'
-    vocal_audio_path = 'non_split_audio/vocal-audio'
-
-    assert len(os.listdir(full_audio_path)) == len(os.listdir(instrumental_audio_path)) == len(os.listdir(vocal_audio_path))
-
-def upload_audio_to_gcs():
+def split_and_upload_audios():
     storage_client = storage.Client.from_service_account_json('/workspace/ODEUM/react-flask-app/backend/api/odeum-421210-44f51ed247e8.json')
     bucket = storage_client.get_bucket("odeum-musics")
+
+    gcs_non_split_audio_path = "non_split_audio/"
+    gcs_split_audio_path = "split_audio/"
+    timestampes_path = "timestamps.txt"
+
+    split_info = read_data(timestampes_path)
     
-    audio_path = "split_audio/"
-    for audio_type in ["full-audio/", "instrumental-audio/", "vocal-audio/"]:
-        audios = os.listdir(audio_path + audio_type)
-        for audio in audios:
-            blob = bucket.blob('audios/' + audio_type + audio)
-            blob.upload_from_filename(audio_path + audio_type + audio)
+    filenames = [file.name[27:] for file in list(bucket.list_blobs(prefix=gcs_non_split_audio_path+"full-audio/"))]
+    for filename in filenames:
+        if bucket.blob(gcs_split_audio_path + 'vocal-audio/' + filename).exists():
+            continue
+
+        download_blob(bucket, gcs_non_split_audio_path+"full-audio/"+filename, "full_tmp.wav")
+        download_blob(bucket, gcs_non_split_audio_path+"instrumental-audio/"+filename, "instr_tmp.wav")
+        download_blob(bucket, gcs_non_split_audio_path+"vocal-audio/"+filename, "vocal_tmp.wav")
+
+        start, end = find_file_info(split_info, filename)
+        start = int(start * 1000)
+        end = int(end * 1000)
+
+        if not start and not end:
+            raise Exception("no timestamp info is found in ", filename)
+
+        try:
+            full_audio = AudioSegment.from_wav("full_tmp.wav")
+            instr_audio = AudioSegment.from_wav("instr_tmp.wav")
+            vocal_audio = AudioSegment.from_wav("vocal_tmp.wav")
+        except:
+            print(f"an error occurred when getting audio {filename}")
+
+        full_audio[start:end].export("full_segment.wav")
+        instr_audio[start:end].export("instr_segment.wav")
+        vocal_audio[start:end].export("vocal_segment.wav")
+
+        full_split_blob = bucket.blob(gcs_split_audio_path + "full-audio/" + filename)
+        instr_split_blob = bucket.blob(gcs_split_audio_path + "instrumental-audio/" + filename)
+        vocal_split_blob = bucket.blob(gcs_split_audio_path + "vocal-audio/" + filename)
+
+        full_split_blob.upload_from_filename("full_segment.wav")
+        instr_split_blob.upload_from_filename("instr_segment.wav")
+        vocal_split_blob.upload_from_filename("vocal_segment.wav")       
+        
+        os.remove("full_segment.wav")
+        os.remove("instr_segment.wav")
+        os.remove("vocal_segment.wav")
+        os.remove("full_tmp.wav")
+        os.remove("instr_tmp.wav")
+        os.remove("vocal_tmp.wav")
 
 def upload_mfccs_to_gcs():
     storage_client = storage.Client.from_service_account_json('/workspace/ODEUM/react-flask-app/backend/api/odeum-421210-44f51ed247e8.json')
     bucket = storage_client.get_bucket("odeum-musics")
 
-    gcs_audio_path = "audios/"
+    gcs_audio_path = "split_audio/"
+    gcs_mfcc_dir_path = "mfccs/"
     tmp_filename = "tmp.wav"
     num_segments= 10
 
     for audio_type in ["full-audio", "instrumental-audio", "vocal-audio"]:
-        data = {
+        json_filename = gcs_mfcc_dir_path + audio_type + ".json"
+        if bucket.blob(json_filename).exists():
+            download_blob(bucket, json_filename, "tmp.json")
+            with open("tmp.json", 'r') as file:
+                data = json.load(file)
+            os.remove("tmp.json")
+        else:
+            data = {
             "mapping": [],
             "mfcc": [],
             "labels": [],
             "filenames": []  # Store the file names
-        }
+            }
         blobs = storage_client.list_blobs("odeum-musics", prefix = gcs_audio_path + audio_type)
         for blob in blobs:
             audio_filename = blob.name.split('/')[-1]
@@ -153,6 +210,7 @@ def upload_mfccs_to_gcs():
             
             elif length != 30 :
                 print(f"Error : {audio_filename}")  
+            
 
             os.remove(tmp_filename)    
 
@@ -179,7 +237,7 @@ def upload_model_output_json_to_gcs():
     timbre_model_path = os.path.join(os.getcwd(), 'mlModels', 'pred_vocal', 'saved_model')
 
     # Save filenames in gcs
-    filenames = [file.name[18:] for file in list(bucket.list_blobs(prefix="audios/full-audio/"))]
+    filenames = [file.name[27:] for file in list(bucket.list_blobs(prefix="non_split_audio/full-audio/"))]
     
     # Initialize json file that will be uploaded to gcs
     model_output_json = {filename: {} for filename in filenames}
@@ -190,9 +248,11 @@ def upload_model_output_json_to_gcs():
     for predict_type in ["genre", "mood", "timbre"]:
         model_path = locals()[f"{predict_type}_model_path"]
         predict_function = globals()[f"predict_{predict_type}"]
+        
         if predict_type == "genre": mfcc_path = gcs_mfcc_path + "full-audio.json"
         elif predict_type == "mood": mfcc_path = gcs_mfcc_path + "instrumental-audio.json"
         else: mfcc_path = gcs_mfcc_path + "vocal-audio.json"
+        print(mfcc_path)
 
         blob = bucket.blob(mfcc_path)
         blob.download_to_filename(tmp_mfcc_filename)
@@ -218,9 +278,8 @@ def upload_model_output_json_to_gcs():
         
 
 if __name__ == "__main__":
-    # before_preprocessing()
-    # split_wav_into_segments()
-    # upload_audio_to_gcs()
-    # upload_mfccs_to_gcs()
+    separate_audios_and_upload()
+    split_and_upload_audios()
+    upload_mfccs_to_gcs()
     upload_model_output_json_to_gcs()
 
